e4e251a2ff7ea5ef10bafb46805a4f2df0de13ce
苏伊 <2323817573@qq.com>
2024-01-05 22:36:13 +0800

    task2

--- ETA_xgboost.py
+++ ETA_xgboost.py
@@ -4,15 +4,13 @@ import pathlib
 from xgboost import XGBRegressor
 
 cwd = pathlib.Path.cwd()
-data = pd.read_csv(cwd / 'data' / 'traj.csv', index_col=0)
+data = pd.read_csv(cwd / 'data' / 'match_output.csv', index_col=0)
 
 def process_data(data):
-    # 将time转化为时间格式，并生成相应的时间戳
     data['time'] = pd.to_datetime(data['time'])
     data['timestamp'] = data['time'].apply(lambda x: x.timestamp())
-    # 将coordinates拆分为经纬度
-    data['end_longitude'] = data['coordinates'].apply(lambda x: eval(x)[0])
-    data['end_latitude'] = data['coordinates'].apply(lambda x: eval(x)[1])
+    data['end_longitude'] = data['matched_coordinates'].apply(lambda x: eval(x)[0])
+    data['end_latitude'] = data['matched_coordinates'].apply(lambda x: eval(x)[1])
 
 def merge_data(group):
     group['start_longitude'] = group['end_longitude'].iloc[0]
@@ -88,30 +86,27 @@ test_x = test_data[features]
 test_pred = model.predict(test_x)
 test_data['time_diff'] = test_pred
 
-# 创建一个新的DataFrame来存储拆分后的数据
-new_rows = []
 
-# 遍历原始DataFrame的每一行
+new_rows = []
 for _, row in test_data.iterrows():
-    start_row = row.copy()  # 复制原始行数据
-    end_row = row.copy()    # 复制原始行数据
-    # 修改第一行的值
+    start_row = row.copy()
+    end_row = row.copy()
+
     start_row['id'] = start_row['start_id']
     start_row['coordinates'] = [start_row['start_longitude'], start_row['start_latitude']]
     start_row['current_dis'] = 0
     start_row['speeds'] = start_row['start_speeds']
     start_row['time'] = datetime.utcfromtimestamp(start_row['timestamp']).strftime('%Y-%m-%dT%H:%M:%SZ')
-    # 修改第二行的值
+
     end_row['id'] = end_row['end_id']
     end_row['coordinates'] = [end_row['end_longitude'], end_row['end_latitude']]
     end_row['speeds'] = end_row['end_speeds']
     end_row['timestamp'] = end_row['timestamp'] + end_row['time_diff']
     end_row['time'] = datetime.utcfromtimestamp(end_row['timestamp']).strftime('%Y-%m-%dT%H:%M:%SZ')
-    # 添加两行到新的DataFrame
+
     new_rows.append(start_row)
     new_rows.append(end_row)
 
-# 创建新的DataFrame
 new_df = pd.DataFrame(new_rows, columns=['id', 'traj_id', 'time', 'entity_id', 'coordinates', 'current_dis',
                                          'speeds', 'holidays'])
 #输出最终预测结果
---
+++ cluster_kmeans.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from tslearn.metrics import dtw
+import numpy as np
+
+df = pd.read_csv('./data/match_output.csv')
+
+df['coordinates'] = df['matched_coordinates'].apply(lambda x: eval(x))
+df['longitude'] = df['coordinates'].apply(lambda x: x[0])
+df['latitude'] = df['coordinates'].apply(lambda x: x[1])
+
+#构建时间序列数据集
+time_series_dataset = df.groupby('traj_id')[['longitude', 'latitude']].apply(lambda x: x.values.tolist()).tolist()
+
+print("计算DTW距离矩阵……")
+len_ = len(time_series_dataset)
+dtw_distance_matrix = np.zeros((len_, len_))
+for i in range(len_):
+    for j in range(i+1, len_):
+        dtw_distance_matrix[i, j] = dtw(time_series_dataset[i], time_series_dataset[j],distance_only=True)
+    progress = (i / len_) * 100
+    print(f"\r进度：{progress:.2f}%", end='', flush=True)
+
+dtw_distance_matrix += dtw_distance_matrix.T
+
+#选取特征并操纵
+df['dtw_distance'] = dtw_distance_matrix.mean(axis=1)
+df['time'] = pd.to_datetime(df['time'])
+df['time_diff'] = df.groupby('traj_id')['time'].diff().dt.total_seconds()
+traj_group_mean_speeds = df.groupby('traj_id')['speeds'].mean()
+df = df.merge(traj_group_mean_speeds.reset_index(name='mean_speed'), on='traj_id')
+
+features = ['longitude', 'latitude', 'dtw_distance', 'mean_speed', 'time_diff']
+
+#标准化特征
+scaler = StandardScaler()
+df[features] = scaler.fit_transform(df[features])
+
+n_clusters = 50
+X = df[features]
+print("\n开始聚类……")
+kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+df['cluster_label'] = kmeans.fit_predict(X)
+
+print("开始绘制……")
+plt.figure(figsize=(12, 8))
+for cluster in range(n_clusters):
+    cluster_data = df[df['cluster_label'] == cluster]
+    for idx, row in cluster_data.iterrows():
+        plt.plot(row['longitude'], row['latitude'], label=f'Cluster {cluster}', color=f'C{cluster}', linewidth=0.5)
+
+plt.title('Trajectories Clustered by K-Means using DTW')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+
+plt.legend()
+plt.show()
